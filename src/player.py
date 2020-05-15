f9dd8dfffc2a1331618a23a84b73edf989cac824
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-05-14 21:41:42 -0600

    Added the printRoom command in Player

--- src/player.py
+++ src/player.py
@@ -1,17 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from items import Item
 
 
-class Player(Item):
+class Player:
     def __init__(self, name, room, items=[]):
         self.name = name
         self.room = room
         self.items = items
 
-    def lose_item:
-
-    def add_item:
+    def printRoom(self):
+        print("##################################################################")
+        print(f"Current Room: {self.room.name}")
+        print("##################################################################")
+        print(f"{self.room.description}")
+        print("##################################################################")
+        print("Items in this room:")
+        self.room.displayItems()
+        print("##################################################################")
 
     def __str__(self):
         return f"Player Name: {self.name} \nPlayers {self.room}"
